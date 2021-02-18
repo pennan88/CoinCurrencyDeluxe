7d1695c9e578789a5863812f62e4d270b144a059
Alexander Fagrelius <erikalexander98@hotmail.com>
2021-02-18 19:24:29 +0100

    Första klick

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,6 +1,12 @@
+const CardOneStatus = document.getElementById("CardOneSatus");
+const CardOneGender = document.getElementById("CardOneGender");
+const CardOneSpecies = document.getElementById("CardOneSpecies");
+const CardOneOrigin = document.getElementById("CardOneOrigin");
+const hidecard = document.getElementById("hidecard");
+
 async function CryptoDailyRates() {
   const url = new URL(`https://rickandmortyapi.com/api/character`);
-  url.searchParams.append("name", "squanchy");
+  // url.searchParams.append("name", Charname);
 
   const response = await fetch(url);
   let URLData = await response.json();
@@ -10,12 +16,21 @@ async function CryptoDailyRates() {
 
 CryptoDailyRates().then(function DailyRates(URLData) {
   console.log(URLData);
+  CardOneStatus.innerHTML = URLData.results[1].status;
+  CardOneGender.innerHTML = URLData.results[1].gender;
+  CardOneSpecies.innerHTML = URLData.results[1].species;
+  CardOneOrigin.innerHTML = URLData.results[1].origin.name;
 });
 
-const CardOne = document.getElementById("cardone");
+const Cardtest = document.getElementById("test");
+const CardOne = document.getElementById("CardboxOne");
 
-CardOne.addEventListener("click", function () {});
-
-function myFunction(event) {
-  alert(event.target.nodeName);
-}
+CardOne.addEventListener("click", function () {
+  if (Cardtest.style.display === "flex") {
+    Cardtest.style.display = "none";
+    hidecard.style.display = "flex";
+  } else {
+    Cardtest.style.display = "flex";
+    hidecard.style.display = "none";
+  }
+});
