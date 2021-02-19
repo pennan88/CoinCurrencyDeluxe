e0291f041111c0dd3a96e77346949a199a660bb3
JakobLarsson <jakob.larsson@live.se>
2021-02-19 16:10:22 +0100

    loop för alla DOM objekt + test kod till click

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,5 +1,7 @@
 //bugg med sista page(34) bara 11 objekt. Lösning nu är mat.random(33)
-console.log(CardArray[0].Card);
+
+
+
 async function homePageApi() {
   let randomNum = Math.floor(Math.random() * 33);
   const url = new URL(`https://rickandmortyapi.com/api/character`);
@@ -11,17 +13,21 @@ async function homePageApi() {
   return URLData;
 }
 
-let randomNum2 = Math.floor(Math.random() * 20);
-homePageApi().then(function ApiData(URLData) {
+
+for(let i = 0; i < 6; i++){
+  let randomNum2 = Math.floor(Math.random() * 20);
+  homePageApi().then(function ApiData(URLData) {
   console.log(URLData);
-  CardOne.Status.innerHTML = URLData.results[randomNum2].status;
-  CardOne.Gender.innerHTML = URLData.results[randomNum2].gender;
-  CardOne.Species.innerHTML = URLData.results[randomNum2].species;
-  CardOne.Origin.innerHTML = URLData.results[randomNum2].origin.name;
-  CardOne.Name.innerHTML = URLData.results[randomNum2].name;
-  CardOne.Card.style.background = `url(${URLData.results[randomNum2].image})`;
+  CardArray[i].Status.innerHTML = URLData.results[randomNum2].status;
+  CardArray[i].Gender.innerHTML = URLData.results[randomNum2].gender;
+  CardArray[i].Species.innerHTML = URLData.results[randomNum2].species;
+  CardArray[i].Origin.innerHTML = URLData.results[randomNum2].origin.name;
+  CardArray[i].Name.innerHTML = URLData.results[randomNum2].name;
+  CardArray[i].Card.style.background = `url(${URLData.results[randomNum2].image})`;
   console.log(randomNum2);
 });
+}
+
 
 CardArray[0].Card.addEventListener("click", function () {
   if (CardArray[0].ForgroundDiv.style.display === "none") {
@@ -52,3 +58,13 @@ CardArray[3].Card.addEventListener("click", function () {
     CardArray[3].ForgroundDiv.style.display = "none";
   }
 });
+
+
+// function myFunction(event) { 
+//   let clicked = event.target.children[1];
+//   if (clicked.style.display === "none") {
+//     clicked.style.display = "flex";
+//   } else {
+//     clicked.style.display = "none";
+//   }
+// }
