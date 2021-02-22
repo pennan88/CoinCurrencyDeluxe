71347c4c1da441ca55575596785d00a874aef473
Alexander Fagrelius <erikalexander98@hotmail.com>
2021-02-22 15:57:43 +0100

    Karusell v5 klar

--- Javascript/search.js
+++ Javascript/search.js
@@ -12,14 +12,67 @@ submitbtn.addEventListener("click", function () {
   character = search.value;
   GetcharacterAPI(character).then(function APIdata(data) {
     console.log(data.results);
+    CreateCard(data.results);
   });
 });
 
-function CreateCard() {
-  const newdiv = document.createElement("div");
-  newdiv.className = "outcard-container";
+function CreateCard(data) {
+  let DELETEALL = document.querySelectorAll(".carousel-item");
+  for (let i = 0; i < DELETEALL.length; i++) {
+    DELETEALL[i].remove();
+  }
 
-  let Parentdiv = document.getElementById("Parent");
+  for (let i = 0; i < data.length; i++) {
+    const carousel = document.createElement("div");
 
-  Parentdiv.appendChild(newdiv);
+    if (i === 0) {
+      carousel.className = "carousel-item active";
+    } else {
+      carousel.className = "carousel-item";
+    }
+
+    let Parentdiv = document.querySelector(".carousel-inner");
+
+    Parentdiv.appendChild(carousel);
+
+    const outcard = document.createElement("div");
+    outcard.className = "outcard-container";
+
+    carousel.appendChild(outcard);
+
+    const imgdiv = document.createElement("div");
+    imgdiv.className = "img-container";
+
+    outcard.appendChild(imgdiv);
+
+    const img = document.createElement("img");
+    img.src = data[i].image;
+    imgdiv.appendChild(img);
+
+    const textdiv = document.createElement("div");
+    textdiv.className = "text-container";
+
+    outcard.appendChild(textdiv);
+
+    const Nametext = document.createElement("h3");
+    const Gendertext = document.createElement("h3");
+    const SpeciesText = document.createElement("h3");
+    const Locatiotext = document.createElement("h3");
+    const Origintext = document.createElement("h3");
+    const Statustext = document.createElement("h3");
+
+    Nametext.innerHTML = `Name: ${data[i].name}`;
+    Gendertext.innerHTML = `Gender: ${data[i].gender}`;
+    SpeciesText.innerHTML = `Species: ${data[i].species}`;
+    Locatiotext.innerHTML = `Location: ${data[i].location.name}`;
+    Origintext.innerHTML = `Origin: ${data[i].origin.name}`;
+    Statustext.innerHTML = `Status: ${data[i].status}`;
+
+    textdiv.appendChild(Nametext);
+    textdiv.appendChild(Gendertext);
+    textdiv.appendChild(SpeciesText);
+    textdiv.appendChild(Locatiotext);
+    textdiv.appendChild(Origintext);
+    textdiv.appendChild(Statustext);
+  }
 }
