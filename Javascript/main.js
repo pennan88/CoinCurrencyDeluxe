4bfad742790907bd144edc07ca80806d37a328fc
Alexander Fagrelius <erikalexander98@hotmail.com>
2021-02-22 09:08:21 +0100

    Click fixad för alla

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,7 +1,5 @@
 //bugg med sista page(34) bara 11 objekt. Lösning nu är mat.random(33)
 
-
-
 async function homePageApi() {
   let RandomPage = Math.floor(Math.random() * 33);
   // console.log("Page: ", RandomPage)
@@ -14,61 +12,30 @@ async function homePageApi() {
   return URLData;
 }
 
-
-for(let i = 0; i < 6; i++){
+for (let i = 0; i < 6; i++) {
   let RandomNum = Math.floor(Math.random() * 20);
   homePageApi().then(function ApiData(URLData) {
-  console.log(URLData);
-  CardArray[i].Status.innerHTML = URLData.results[RandomNum].status;
-  CardArray[i].Gender.innerHTML = URLData.results[RandomNum].gender;
-  CardArray[i].Species.innerHTML = URLData.results[RandomNum].species;
-  CardArray[i].Origin.innerHTML = URLData.results[RandomNum].origin.name;
-  CardArray[i].Name.innerHTML = URLData.results[RandomNum].name;
+    console.log(URLData);
+    CardArray[i].Status.innerHTML = URLData.results[RandomNum].status;
+    CardArray[i].Gender.innerHTML = URLData.results[RandomNum].gender;
+    CardArray[i].Species.innerHTML = URLData.results[RandomNum].species;
+    CardArray[i].Origin.innerHTML = URLData.results[RandomNum].origin.name;
+    CardArray[i].Name.innerHTML = URLData.results[RandomNum].name;
 
-  CardArray[i].Card.style.background = `url(${URLData.results[RandomNum].image})`;
-  CardArray[i].Card.style.backgroundPosition = "center";
-  CardArray[i].Card.style.backgroundSize = "cover";
-  console.log(RandomNum);
-});
+    CardArray[
+      i
+    ].Card.style.background = `url(${URLData.results[RandomNum].image})`;
+    CardArray[i].Card.style.backgroundPosition = "center";
+    CardArray[i].Card.style.backgroundSize = "cover";
+    console.log(RandomNum);
+  });
 }
 
-
-CardArray[0].Card.addEventListener("click", function () {
-  if (CardArray[0].ForgroundDiv.style.display === "none") {
-    CardArray[0].ForgroundDiv.style.display = "flex";
+function clickFunction(card) {
+  const b = card.getElementsByClassName("Forground")[0];
+  if (b.style.display === "none") {
+    b.style.display = "flex";
   } else {
-    CardArray[0].ForgroundDiv.style.display = "none";
+    b.style.display = "none";
   }
-});
-
-CardArray[1].Card.addEventListener("click", function () {
-  if (CardArray[1].ForgroundDiv.style.display === "none") {
-    CardArray[1].ForgroundDiv.style.display = "flex";
-  } else {
-    CardArray[1].ForgroundDiv.style.display = "none";
-  }
-});
-CardArray[2].Card.addEventListener("click", function () {
-  if (CardArray[2].ForgroundDiv.style.display === "none") {
-    CardArray[2].ForgroundDiv.style.display = "flex";
-  } else {
-    CardArray[2].ForgroundDiv.style.display = "none";
-  }
-});
-CardArray[3].Card.addEventListener("click", function () {
-  if (CardArray[3].ForgroundDiv.style.display === "none") {
-    CardArray[3].ForgroundDiv.style.display = "flex";
-  } else {
-    CardArray[3].ForgroundDiv.style.display = "none";
-  }
-});
-
-
-// function myFunction(event) { 
-//   let clicked = event.target.children[1];
-//   if (clicked.style.display === "none") {
-//     clicked.style.display = "flex";
-//   } else {
-//     clicked.style.display = "none";
-//   }
-// }
+}
